75a8a2a0b7eb96a93abf1b2e7e106de61826d59d
dlehdals6122-hash <dlehdals6122@gmail.com>
2026-08-14 23:41:39 +0900

    온라인게임 업뎃
    온라인게임 업뎃

--- data.js
+++ data.js
@@ -74,7 +74,30 @@ const quizDatabase = {
         { imageUrl: "https://flagcdn.com/w320/za.png", answers: ["남아프리카공화국", "남아공"] },
     ],
 
-    // 2. 나중에 추가할 게임을 위한 빈 공간 (미리 만들어 둡니다)
-    pokemon: [],
-    football: [] 
+   // ⬇️ 여기에 새로운 게임 데이터를 추가하세요!
+    games: [
+       { answers: ["메이플스토리", "메이플"], imageUrl: "images/onlinegame/maplestory.png" },
+       { answers: ["카트라이더", "카트"], imageUrl: "images/onlinegame/kart.png" },
+       { answers: ["리그오브레전드", "리오레", "롤"], imageUrl: "images/onlinegame/lol.png" },
+       { answers: ["크레이지아케이드", "크아"], imageUrl: "images/onlinegame/ca.png" },
+       { answers: ["바람의나라", "바람"], imageUrl: "images/onlinegame/baram.png" },
+       { answers: ["스톤에이지", "스톤"], imageUrl: "images/onlinegame/stoneage.png" },
+        { answers: ["배틀그라운드", "배그"], imageUrl: "images/onlinegame/battleground.png" },
+         { answers: ["오버워치", "옵치"], imageUrl: "images/onlinegame/overwatch.png" },
+         { answers: ["던전앤파이터", "던파"], imageUrl: "images/onlinegame/dnf.png" },
+         { answers: ["서든어택", "서든"], imageUrl: "images/onlinegame/suddenattack.png" },
+         { answers: ["마비노기", "마비"], imageUrl: "images/onlinegame/mabinogi.png" },
+         { answers: ["테일즈런너", "테일즈러너", "테런", "테러"], imageUrl: "images/onlinegame/talesrunner.png" },
+         { answers: ["알투비트", "알투", "알비", "알빗"], imageUrl: "images/onlinegame/r2beat.png" },
+         { answers: ["천하제일상 거상", "거상"], imageUrl: "images/onlinegame/gusang.png" },
+         { answers: ["로스트아크", "로아"], imageUrl: "images/onlinegame/rostark.png" },
+         { answers: ["아이온"], imageUrl: "images/onlinegame/aion.png" },
+         { answers: ["월드 오브 워크래프트", "와우", "WOW", "월오워"], imageUrl: "images/onlinegame/wow.png" },
+         { answers: ["사이퍼즈", "싸이퍼즈", "사퍼", "싸퍼"], imageUrl: "images/onlinegame/cypers.png" },
+         { answers: ["원신"], imageUrl: "images/onlinegame/genshin.png" },
+         { answers: ["이터널 리턴"], imageUrl: "images/onlinegame/eternalreturn.png" },
+         { answers: ["발로란트", "발로"], imageUrl: "images/onlinegame/valo.png" },
+         { answers: ["로블록스", "로블"], imageUrl: "images/onlinegame/roblox.png" },
+         { answers: ["팰월드", "펠월드"], imageUrl: "images/onlinegame/palworld.png" },
+    ]
 };
